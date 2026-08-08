38f0404eaedcd11b5cbb35211b2ad074fb02c143
Aditya Gawande <ee22btech11202@iith.ac.in>
2026-08-08 14:47:50 +0530

    Comments added to S3_wrapper

---
+++ S3_wrapper.py
@@ -0,0 +1,8 @@
+# This should update data from google sheet
+# Enter testmode and wait to verify that testmode has been activated
+# Measure voltage at VREF using the same SMU
+# Write bits to the chip and verify that bits have been changed
+# Exit testmode
+# Measure gain/cmrr based on user input for n times
+# Update value into google sheet (manually)
+# Loopback

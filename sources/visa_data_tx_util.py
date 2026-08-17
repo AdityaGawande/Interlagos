736b0f3b3c990f2eaa04568c47e1503cee6024e0
Aditya Gawande <ee22btech11202@iith.ac.in>
2026-08-17 22:53:04 +0530

    I2C trial

--- sources/visa_data_tx_util.py
+++ sources/visa_data_tx_util.py
@@ -57,20 +57,20 @@ def run_smu_sequence_v2(length, arr_high, arr_low):
         # input("Wait and check, before clock")
         time.sleep(time_period/2)
 
-        with ThreadPoolExecutor() as executor:
-                executor.submit(afg.write, "*TRG\n")
-                executor.submit(SMU_high_transition, smu, arr_high, i)
+        # with ThreadPoolExecutor() as executor:
+        #         executor.submit(afg.write, "*TRG\n")
+        #         executor.submit(SMU_high_transition, smu, arr_high, i)
                 # executor.submit(dmm3.write, "*TRG\n")
         
-        # afg.write("*TRG \n")
+        afg.write("*TRG \n")
         # There should be some delay between the AFG trigger and SMU data,
         # but the SMU takes some time to reflect the LAN command, so this works out.
-        # time.sleep(time_period_test/4)
+        time.sleep(time_period/4 - correction)
         
-        # if arr_high[i]:
-        #     smu.write(f"smua.source.levelv = {high_level}")
-        # else:
-        #     smu.write(f"smua.source.levelv = {low_level}")
+        if arr_high[i]:
+            smu.write(f"smua.source.levelv = {high_level}")
+        else:
+            smu.write(f"smua.source.levelv = {low_level}")
         time.sleep(time_period/2)
         
         # input("Data transition here")
@@ -79,12 +79,12 @@ def run_smu_sequence_v2(length, arr_high, arr_low):
     # print("SMU sequence complete.")
 
 
-def SMU_high_transition(smu, arr_high, i):
-    time.sleep(time_period/4 - correction)
-    if arr_high[i]:
-        smu.write(f"smua.source.levelv = {high_level}")
-    else:
-        smu.write(f"smua.source.levelv = {low_level}")
+# def SMU_high_transition(smu, arr_high, i):
+#     time.sleep(time_period/4 - correction)
+#     if arr_high[i]:
+#         smu.write(f"smua.source.levelv = {high_level}")
+#     else:
+#         smu.write(f"smua.source.levelv = {low_level}")
 
 
 def run_smu_sequence(length, arr_high, arr_low, t=0.00025):
